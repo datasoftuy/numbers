0626969532dcb816804edec1506785a406b57082
datasoftuy <andres.esteves.aharonian@gmail.com>
2017-05-07 21:18:01 -0300

    test1

---
+++ Num/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+
+
+            Console.WriteLine("Ingrese numero1");
+            num1=Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine("Ingrese numero 2 distinto de  0 ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            
+
+            if (num2 == 0)
+            {
+                Console.WriteLine("El numero debe de ser 0 ");
+            }
+            else
+                
+                Console.WriteLine("El resultado es :"  + (num1 / num2 ));
+
+            Console.ReadLine();
+        
+        
+        
+        }
+    }
+}
